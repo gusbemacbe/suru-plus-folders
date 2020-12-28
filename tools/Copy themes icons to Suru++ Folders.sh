c33c469201923d2d3344be4a5aabba9d351b55c1
Gustavo Costa <gusbemacbe@gmail.com>
2020-12-28 03:10:27 -0300

    Executável modificado

--- tools/Copy themes icons to Suru++ Folders.sh
+++ tools/Copy themes icons to Suru++ Folders.sh
@@ -1,5 +1,5 @@
-CAMINHO="$HOME/GitHub/adwaita-plus-private/Adwaita++/places/scalable"
-DESTINO="$HOME/GitHub/suru-plus-folders/images/adwaita-plus" 
+CAMINHO="$HOME/GitHub/Gustavo/adwaita-plus-private-gusbemacbe/Adwaita++/places/scalable"
+DESTINO="$HOME/GitHub/Gustavo/suru-plus-folders/images/adwaita-plus" 
 
 cp $CAMINHO/folder-{adwaita,aubergine,black,blue,bluegrey,bordeaux,brown,canonical,custom,green,grey,indigo,manjaro,mint,orange,purple,red,white,yaru,yellow}-{app,documents,downloads,dropbox,gitlab,music,open,pictures,visiting}.svg $DESTINO
 
